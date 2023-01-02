71d74005b9e09e0fe455573eec8dec82f3090849
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2023-01-02 09:09:15 -0700

    added uniswap ABIs

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // this will load our environment variables
 require('dotenv').config();
+const ethers = require("ethers");
 
+// uni/sushiswap ABIs
+const UniswapV2Pair = require('./abis/IUniswapV2Pair.json');
+const UniswapV2Factory = require('./abis/IUniswapV2Factory.json');
